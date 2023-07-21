f00f5ab0316517da30e298fbe70af13b4862aa14
Arthur Melo <arthur7m@gmail.com>
2023-07-21 09:50:21 -0300

    Criado primeira funcionalidade da aplicação

--- app/app.go
+++ app/app.go
@@ -1,11 +1,44 @@
 package app
 
-import "github.com/urfave/cli"
+import (
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/urfave/cli"
+)
 
 func Gerar() *cli.App {
 	app := cli.NewApp()
 	app.Name = "Aplicação de linha de comando"
-	app.Usage = "Busca IPs e nomes de Servidor na internet"
+	app.Usage = "Busca IPs e nomes de Servidores na internet"
+
+	app.Commands = []cli.Command {
+		{
+			Name: "ip",
+			Usage: "Busca ips de endereços na internet",
+			Flags: []cli.Flag {
+				cli.StringFlag{
+					Name: "host",
+					Value: "",
+				},
+			},
+			Action: buscarIps,
+		},
+	}
 
 	return app
+}
+
+func buscarIps(c *cli.Context) {
+	host := c.String("host")
+
+	ips, erro := net.LookupIP(host)
+	if erro != nil {
+		log.Fatal(erro)
+	}
+
+	for _, ip := range ips {
+		fmt.Println(ip)
+	}
 }
