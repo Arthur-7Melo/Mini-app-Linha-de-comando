bddfc8e9faa62d5abd6eb90a497762f8d4568226
Arthur Melo <arthur7m@gmail.com>
2023-07-21 10:32:09 -0300

    Criado segunda funcionalidade da aplicação

--- app/app.go
+++ app/app.go
@@ -12,18 +12,26 @@ func Gerar() *cli.App {
 	app := cli.NewApp()
 	app.Name = "Aplicação de linha de comando"
 	app.Usage = "Busca IPs e nomes de Servidores na internet"
+	flag := []cli.Flag {
+		cli.StringFlag{
+			Name: "host",
+			Value: "",
+		},
+	}
 
 	app.Commands = []cli.Command {
 		{
 			Name: "ip",
 			Usage: "Busca ips de endereços na internet",
-			Flags: []cli.Flag {
-				cli.StringFlag{
-					Name: "host",
-					Value: "",
-				},
-			},
+			Flags: flag,
 			Action: buscarIps,
+		},
+
+		{
+			Name: "servidor",
+			Usage: "Busca o nome de servidores na internet",
+			Flags: flag,
+			Action: buscarServidores,
 		},
 	}
 
@@ -40,5 +48,18 @@ func buscarIps(c *cli.Context) {
 
 	for _, ip := range ips {
 		fmt.Println(ip)
+	}
+}
+
+func buscarServidores(c *cli.Context) {
+	host := c.String("host")
+
+	servidores, erro := net.LookupNS(host)
+	if erro != nil {
+		log.Fatal(erro)
+	}
+
+	for _, servidor := range servidores {
+		fmt.Println(servidor.Host)
 	}
 }
